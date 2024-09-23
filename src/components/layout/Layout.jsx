68e92672fdbdf7b03d1c71e7e7d7630344596a1e
Ailton Macedo <ailtonwsb@gmail.com>
2024-09-23 00:41:49 -0300

    refactor: layout adjustments

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -9,21 +9,15 @@ function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>BigMinds Tech | Transformamos Ideias em Inovação.</title>
-        <meta
-          name="description"
-          content="Empresa focada em transaformar ideias em Inovação."
-        />
         <link rel="icon" href="/assets/img/sm-logo.svg" />
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:site_name" content="BigMinds Tech" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.bigminds.tech/" />
         <meta property="og:title" content="Página não encontrada" />
         <meta
           property="og:description"
-          content="Soluções personalizadas em cobrança e recuperação de crédito para sua empresa."
+          content="Empresa focada em transaformar ideias em Inovação."
         />
         <meta
           property="og:image"
